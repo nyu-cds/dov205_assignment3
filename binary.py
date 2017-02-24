66dbd27e9b504adef803ccc1108191635ae8ac37
Danny Vilela <dov205@nyu.edu>
2017-02-24 16:17:43 -0500

    [upload] binary, zbits, documentation

---
+++ binary.py
@@ -0,0 +1,27 @@
+from itertools import product, filterfalse
+
+
+def zbits(n: int, k: int) -> set():
+    """Generate :n-length binary sequences with :k zero-bits.
+
+    :param n: the length of our desired output sequences.
+    :param k: the number of zero-bits allowed in our output.
+    :return as_strings: set of binary sequences as strings that fulfill :n and :k constraints.
+    """
+    
+    # TODO can be optimized? No need to generate ALL pairs.
+    # Generate all :n-length pairs, choosing from either 0 or 1.
+    all_pairs = product([0, 1], repeat = n)
+
+    # Calculate the difference beteween :n and :k to avoid re-computation.
+    difference = n - k
+
+    # Filter our initial space by removing products where k + number of zeros != n.
+    filtered = filterfalse(lambda outcome: sum(outcome) != difference, all_pairs)
+
+    # Join our valid binary sequence sets into their joined string representation.
+    as_strings = {"".join(str(bit) for bit in outcome) for outcome in filtered}
+
+    # Output: {"101...", "011...", ...}
+    return as_strings
+
