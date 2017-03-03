f32b103ddb974dae9eb829975b89d9d9ea183f15
Danny Vilela <dov205@nyu.edu>
2017-03-02 19:46:05 -0500

    [update] further optimize numpy functions

--- calculator.py
+++ calculator.py
@@ -1,22 +1,23 @@
 """
     Name: Danny Vilela (dov205)
     
-    Original runtime: Average(1.37s, 1.38s, 1.38s) =  1.376667s
-    Improved runtime: Average(31ms, 31.1ms, 31ms)  = 31.033333ms = 0.0310s
-    Relative speedup: (1.376667 / 0.0310) = 44.40
+    Original runtime: Average(1.37s, 1.38s, 1.38s)    =  1.376667s
+    Improved runtime: Average(25.3ms, 25.4ms, 25.3ms) = 25.333333ms = 0.0253333s
+    Relative speedup: (1.376667 / 0.0253333) = 54.342
 
     Comments:
-        The majority of the task was using NumPy-native functions
-        as opposed to using user-defined functions. Under the hood,
-        NumPy uses numerous optimizations in their dot(), add(), and sqrt()
-        functions. 
+        The majority of the task was using user-defined functions that 
+        are (optimally) implemented as NumPy-native functions. In fact,
+        during my first iteration of calculator.py I replaced the following:
         
-        That said, there's no reason to reinvent the wheel. The explicit
-        changes were as follow:
+            multiple() -> np.multiply()
+            add()      -> np.add()
+            sqrt()     -> np.sqrt()
 
-        multiple() -> np.multiply()
-        add()      -> np.add()
-        sqrt()     -> np.sqrt()
+        This had a huge speedup of approximately 44x relative to the base
+        implementation. After some investigation, however, it turns out the 
+        NumPy API actually has a function for computing the hypotenuse! After
+        implementing that solution, I achieved a relative speedup of 54x.
 
     Evaluated from IPython shell with the following:
 
@@ -32,23 +33,14 @@
 # calculator.py
 # ----------------------------------------------------------------------------- 
 
-def hypotenuse(x,y):
-    """
-    Return sqrt(x**2 + y**2) for two arrays, a and b.
-    x and y must be two-dimensional arrays of the same shape.
-    """
-    xx = multiply(x, x)
-    yy = multiply(y, y)
-    zz = add(xx, yy)
-    return sqrt(zz)
-
+# Empty! Who needs it.
 
 # -----------------------------------------------------------------------------
 # calculator_test.py
 # ----------------------------------------------------------------------------- 
 if __name__ == '__main__':
     
-    from numpy import multiply, add, sqrt 
+    from numpy import hypot
     from numpy.random import random
 
     M = 10**3
@@ -56,4 +48,5 @@ if __name__ == '__main__':
 
     A = random((M,N))
     B = random((M,N))
-    C = hypotenuse(A,B)
+    C = hypot(A,B)
+
